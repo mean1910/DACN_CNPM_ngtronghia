015a2eb364a58e7d6a6949b9d51e4ab94b2dbafb
mean1910 <nguyentrongnghia19102003@gmail.com>
2024-10-17 17:26:50 +0700

    them lai template

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace elearning_b1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
