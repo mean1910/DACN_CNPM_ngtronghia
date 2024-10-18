762d209e2cf1fb9c2a5ce33a7bd7d682addebd1a
mean1910 <nguyentrongnghia19102003@gmail.com>
2024-10-18 21:32:08 +0700

    dang ky dang nhap

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace elearning_b1.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
