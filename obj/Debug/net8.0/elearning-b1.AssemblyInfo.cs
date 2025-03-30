49735a18d535b1ea135abb33769a322e8b7c31c5
mean1910 <nguyentrongnghia19102003@gmail.com>
2025-03-30 16:11:32 +0700

    cap nhat

--- obj/Debug/net8.0/elearning-b1.AssemblyInfo.cs
+++ obj/Debug/net8.0/elearning-b1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("elearning-b1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67c477f7013e385f7357abbd1777cb6d1f29785e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c8b0146a4592f263eeb97eac92f195e7bf63e3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("elearning-b1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("elearning-b1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
